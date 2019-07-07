f96d0a7bea87718e6c6e0ba6c855da90d3dde4af
Kevin Giorgio Toledo De La cruz <kgtoledoc19@gmail.com>
2019-07-07 09:04:00 -0500

    Giving children component access to state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const Timer = ({ currentValue, resetTimerFunction }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Timer" onClick={resetTimerFunction}>
+      {currentValue}
     </div>
-  );
+  )
+
+}
+
+//function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentValue: 150 }
+
+    setInterval(() => {
+      this.setState({
+        currentValue: this.state.currentValue - 1
+      })
+    }, 1000);
+
+  }
+  // Reset the current value to 150
+  resetCurrent = () => {
+    this.setState({
+      currentValue: 150
+    })
+  }
+  render() {
+    return (
+      <div className="App" >
+
+        <Timer currentValue={this.state.currentValue}
+          resetTimerFunction={this.resetCurrent}></Timer>
+      </div>
+
+    )
+  };
 }
 
 export default App;
